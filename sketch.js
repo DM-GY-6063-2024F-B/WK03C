4d175f839d528d79feea20d78959421c6aaf3d34
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-09-24 00:00:59 -0400

    after class

--- sketch.js
+++ sketch.js
@@ -1,6 +1,34 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+function star(x, y, w, c) {
+  let r = w / 2;
+  push();
+  translate(x, y);
+  line(-c, -c, 0, -r);
+  line(0, -r, c, -c);
+
+  line(-c, c, 0, r);
+  line(0, r, c, c);
+
+  line(c, -c, r, 0);
+  line(r, 0, c, c);
+
+  line(-c, -c, -r, 0);
+  line(-r, 0, -c, c);
+  pop();
 }
 
-function draw() {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(220, 120, 120);
+
+  strokeWeight(2);
+
+  // draw a column of rows
+  for (let y = 50; y < height; y += 100) {
+    // draw a row of stars
+    for (let x = 50; x < width; x += 100) {
+      star(x, y, 100, 15);
+    }
+  }
 }
+
+function draw() {}
